24aa85bc2994519630e007ffec05a45f13f17fc7
Alyona <alyona@MacBook-Pro-User.local>
2017-09-05 23:32:39 +0500

    Initial Commit

---
+++ Unilot/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Unilot
+//
+//  Created by Alyona on 9/5/17.
+//  Copyright © 2017 Vovasoft. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
